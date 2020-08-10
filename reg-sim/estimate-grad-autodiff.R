0f9f36d15fd7ea6716ff9a0dd5ddc064c4f7eb7d
Radford Neal <radfordneal@gmail.com>
2020-08-09 21:50:42 -0400

    revised GD / autodiff estimation

--- reg-sim/estimate-grad-autodiff.R
+++ reg-sim/estimate-grad-autodiff.R
@@ -1,6 +1,8 @@
 # Code for model parameter estimation by gradient descent, 
 # using autodiff (pqR only).
 #
+# Updates parameters in P_init to new values in P_new.
+#
 # Copyright 2020 by Radford M. Neal
 # 
 # This program is free software; you can redistribute it and/or modify
@@ -17,77 +19,52 @@
 # along with this program; if not, a copy is available at
 # https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
-N_evals <- 0
+cat("Number of parameters:",sum(sapply(P_init,length)),"\n")
 
-opt <- function (x)
+if (TRUE)  # can be disabled for debugging
 { 
-  N_evals <<- N_evals + 1
+  eta <- 0*P_init + 5e-4
+  eta$mc[18:23] <- 5e-5
+  eta$imm_decay <- 1e-6
+  eta$ltimm_decay <- 1e-7
+  eta$Rt_offset_alpha <- 1e-6
+  eta$Rt_offset_sd <- 1e-7
 
-  with gradient (x)
-  { 
-    P <- P_init
-    # P$Rt_offset_sd <- exp(x[1])
-    # P$Rt_offset_alpha <- tanh(x[2])
-  
-    P$mc[18:23] <- x / 100
-  
-    # cat("OPT CALLED, with\n  ",round(P$mc,3),"\n")
-    # cat("diff from P_init is\n  ",P$mc-P_init$mc,"\n")
-  
-    ws <- run_sims (subn, full=nsims, subset=high, P=P, cache=cache, info=FALSE)
-    tws <- itrans_wsims(ws)
-    result <- - profile_log_lik (tws, full=nsims)
-    # cat("result is",result,"\n")
-    # cat("gradient of result is",gradient_of(result),"\n")
-  
-    result
-  }
-}
+  alpha <- 0.99
 
-# x_init <- c (log(P_init$Rt_offset_sd),
-#              atanh(P_init$Rt_offset_alpha))
+  p <- 0*P_init
+  P_new <- P_init
 
-x_init <- P_init$mc[18:23] * 100
-
-if (TRUE)  # can enable for debugging
-{ cat("Initial parameter vector:\n")
-  print(x_init)
-  opt_value <- opt(x_init)
-  cat("Initial opt value:",round(opt_value,3),"\n")
-  cat("Gradient of initial opt value:",attr(opt_value,"gradient"),"\n")
-  if (FALSE)
-  { delta <- 1e-7
-    cat("With",delta,"changes:\n")
-    for (i in 1:length(x_init)) 
-    { print(opt(x_init+delta*((1:length(x_init))==i)))
-    }
-    for (i in 1:length(x_init)) 
-    { print(opt(x_init-delta*((1:length(x_init))==i)))
-    }
-  }
-  cat("\n")
-  N_evals <- 0
-}
-
-P_new <- P_init
-
-if (FALSE) x_new <- x_init  # for debugging
-else
-{ eta <- 4e-5
-  alpha <- 0.95
-  x_new <- x_init
-  p <- 0
   for (iter in 1:100)
-  { o <- opt(x_new)
-    p <- alpha*p - eta * attr(o,"gradient")
-    x_new <- x_new + p
-    cat("iteration",iter,"value",round(o,5),"\n")
+  { 
+    nll <- with gradient (P_new)
+    { ws <- run_sims (subn, full=nsims, subset=high, P=P_new, 
+                      cache=cache, info=FALSE)
+      tws <- itrans_wsims(ws)
+      - profile_log_lik (tws, full=nsims)
+    }
+
+    this_eta <- if (iter<15) 0.2*eta else eta
+    this_alpha <- if (iter<15) 0 else if (iter<30) alpha^2 else alpha
+
+    g <- attr(nll,"gradient")
+    p <- p - this_eta * g
+    P_new <- P_new + this_eta * p
+
+    K <- sum(sapply(p^2,sum))/2
+    
+    cat ("iteration",iter,
+         ": log lik",round(-nll,5),
+         ": K",round(K,5),
+         ": energy",round(nll+K,5))
+    if (this_alpha>0)
+    { cat(" ->",round(nll+this_alpha^2*K,5))
+    }
+    cat("\n")
+
+    p <- if (iter<20) 0 else alpha*p
   }
+
+  cat("\nChange in parameter values:\n")
+  print(P_new-P_init)
 }
-
-# P_new$Rt_offset_sd <- exp(estim[1])
-# P_new$Rt_offset_alpha <- tanh(estim[2])
-
-P_new$mc[18:23] <- x_new / 100
-
-cat("Number of function evaluations:",N_evals,"\n")
